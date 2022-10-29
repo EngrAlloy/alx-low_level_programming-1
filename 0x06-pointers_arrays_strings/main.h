cc064455615f6631332ca94c61eaef7da2a160c9
ANIEFIOK STEPHEN ESSIET <114703561+EngrAlloy@users.noreply.github.com>
2022-10-29 01:42:24 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,11 +17,11 @@ int _strcmp(char *s1, char *s2);
 
 void reverse_array(int *a, int n);
 
-char *string_toupper(char *);
+char *string_toupper(char *n);
 
-char *cap_string(char *);
+char *cap_string(char *str);
 
-char *leet(char *);
+char *leet(char *n);
 
 char *rot13(char *);
 
